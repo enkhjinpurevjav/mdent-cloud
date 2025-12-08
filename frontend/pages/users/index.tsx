a964798a58c904f4bfa7379533e246b5a6d7013f
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-08 14:06:40 +0800

    Create index.tsx

---
+++ frontend/pages/users/index.tsx
@@ -0,0 +1,4 @@
+<li><Link href="/users/doctors">Эмч</Link></li>
+<li><Link href="/users/receptionists">Ресепшн</Link></li>
+<li><Link href="/users/nurses">Сувилагч</Link></li>
+<li><Link href="/users/staff">Бусад ажилтан</Link></li>
