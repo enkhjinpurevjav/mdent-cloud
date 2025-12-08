e54a0f153b8c7dd9c0e6dbb60d349e9fcf04776f
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-08 13:32:29 +0800

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,8 +6,8 @@ export default function Dashboard() {
       <h1>🦷 M Дент хянах самбар</h1>
       <nav>
         <ul>
-          <li><Link href="/patients">Үйлчлүүлэгчийн бүртгэл</Link></li>
           <li><Link href="/appointments">Цаг захиалга</Link></li>
+          <li><Link href="/patients">Үйлчлүүлэгчийн бүртгэл</Link></li>
           <li><Link href="/encounters">Үзлэг</Link></li>
           <li><Link href="/billing">Төлбөр</Link></li>
           <li><Link href="/users">Ажилтан</Link></li>
