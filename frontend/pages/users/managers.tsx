d4e4e1efb4e4712cc122fadcb790c56e8dd59815
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-08 16:54:47 +0800

    Create managers.tsx

---
+++ frontend/pages/users/managers.tsx
@@ -0,0 +1,11 @@
+import StaffPage from "../../components/StaffPage";
+
+export default function ManagersPage() {
+  return (
+    <StaffPage
+      role="manager"
+      title="Менежерүүд"
+      createTitle="Шинэ менежер бүртгэх"
+    />
+  );
+}
