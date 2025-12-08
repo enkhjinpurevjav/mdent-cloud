022b8a687432cfce5850c2e273813cf083e2c024
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-08 16:52:22 +0800

    Update title for ReceptionistsPage component

---
+++ frontend/pages/users/receptionists.tsx
@@ -0,0 +1,11 @@
+import StaffPage from "../../components/StaffPage";
+
+export default function ReceptionistsPage() {
+  return (
+    <StaffPage
+      role="receptionist"
+      title="Ресепшн"
+      createTitle="Шинэ ресепшн бүртгэх"
+    />
+  );
+}
