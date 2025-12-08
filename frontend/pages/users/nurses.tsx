7072a8e381425c13d2473d85e7b3546e1508e6d5
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-08 16:52:53 +0800

    Add NursesPage component for nurse management

---
+++ frontend/pages/users/nurses.tsx
@@ -0,0 +1,11 @@
+import StaffPage from "../../components/StaffPage";
+
+export default function NursesPage() {
+  return (
+    <StaffPage
+      role="nurse"
+      title="Сувилагчид"
+      createTitle="Шинэ сувилагч бүртгэх"
+    />
+  );
+}
