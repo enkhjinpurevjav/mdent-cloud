f3686b079d11b0170f9de97bcd620a8d94425637
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-08 16:54:26 +0800

    Create accountants.tsx

---
+++ frontend/pages/users/accountants.tsx
@@ -0,0 +1,11 @@
+import StaffPage from "../../components/StaffPage";
+
+export default function AccountantsPage() {
+  return (
+    <StaffPage
+      role="accountant"
+      title="Нярав / Нягтлангууд"
+      createTitle="Шинэ нярав / нягтлан бүртгэх"
+    />
+  );
+}
