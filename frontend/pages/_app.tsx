928a651dace80234c377ec28ba5e2bb9afec3f94
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-21 20:30:29 +0800

    Add custom App component with AdminLayout

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from "next/app";
+import AdminLayout from "../components/AdminLayout";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AdminLayout>
+      <Component {...pageProps} />
+    </AdminLayout>
+  );
+}
