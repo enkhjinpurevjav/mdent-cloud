d99d7b3aae43017b0d6f02c1d804e3f37743baa8
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-22 00:12:35 +0800

    Create ongoing.tsx

---
+++ frontend/pages/visits/ongoing.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminLayout from "../../components/AdminLayout";
+
+export default function OngoingVisitsPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{ fontSize: 20, fontWeight: 600, marginBottom: 12 }}>
+        Үзлэг хийж буй
+      </h1>
+      <p>Энд одоо явж байгаа үзлэгүүдийн жагсаалт гарна (TODO).</p>
+    </AdminLayout>
+  );
+}
