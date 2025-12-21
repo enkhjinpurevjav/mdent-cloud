971b738aac42e5d675f9009b2c1a7e221f1052fb
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-22 00:11:16 +0800

    Create BookedVisitsPage component
    This page will display a list of today's booked visits.

---
+++ frontend/pages/visits/booked.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminLayout from "../../components/AdminLayout";
+
+export default function BookedVisitsPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{ fontSize: 20, fontWeight: 600, marginBottom: 12 }}>
+        Цаг захиалсан
+      </h1>
+      <p>Энд өнөөдрийн цаг захиалсан өвчтөний жагсаалт гарна (TODO).</p>
+    </AdminLayout>
+  );
+}
