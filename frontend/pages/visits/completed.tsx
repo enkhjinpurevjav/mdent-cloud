23862203291f2821f7078be4de8e6417f02369dc
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-22 10:33:33 +0800

    Remove AdminLayout from CompletedVisitsPage

--- frontend/pages/visits/completed.tsx
+++ frontend/pages/visits/completed.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import AdminLayout from "../../components/AdminLayout";
 
 export default function CompletedVisitsPage() {
   return (
-    <AdminLayout>
+    <>
       <h1 style={{ fontSize: 20, fontWeight: 600, marginBottom: 12 }}>
         Дууссан
       </h1>
-      <p>Энд өнөөдөр дууссан/төлбөр төлсөн үзлэгүүдийн жагсаалт гарна (TODO).</p>
-    </AdminLayout>
+      <p>Энд дууссан/төлбөр төлсөн үзлэгүүдийн жагсаалт гарна (TODO).</p>
+    </>
   );
 }
