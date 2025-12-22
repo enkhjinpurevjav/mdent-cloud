900c4238bc9be8a126125a802ad7108ba12d8258
enkhjinpurevjav <enkhjinp@gmail.com>
2025-12-22 10:33:12 +0800

    Update booked.tsx

--- frontend/pages/visits/booked.tsx
+++ frontend/pages/visits/booked.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import AdminLayout from "../../components/AdminLayout";
 
 export default function BookedVisitsPage() {
   return (
-    <AdminLayout>
+    <>
       <h1 style={{ fontSize: 20, fontWeight: 600, marginBottom: 12 }}>
         Цаг захиалсан
       </h1>
-      <p>Энд өнөөдрийн цаг захиалсан өвчтөний жагсаалт гарна (TODO).</p>
-    </AdminLayout>
+      <p>Энд цаг захиалсан өвчтөний жагсаалт гарна (TODO).</p>
+    </>
   );
 }
