f7bca16c84d91fa796353cef32cdd5d16669d7b1
enkhjinpurevjav <enkhjinp@gmail.com>
2026-01-05 12:43:03 +0800

    Update reports.tsx

--- frontend/pages/sterilization/reports.tsx
+++ frontend/pages/sterilization/reports.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import AdminLayout from "../../components/AdminLayout";
+
 
 export default function SterilizationReportsPage() {
   return (
-    <AdminLayout>
+
       <h1 style={{ fontSize: 18 }}>Ариутгалын тайлан</h1>
       <div style={{ color: "#6b7280", fontSize: 13 }}>
         (Дараагийн алхамд хийнэ)
       </div>
-    </AdminLayout>
+
   );
 }
