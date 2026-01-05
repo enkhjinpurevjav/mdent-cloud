216ff1e90ef1d52137c858299e1d72257cf97b59
enkhjinpurevjav <enkhjinp@gmail.com>
2026-01-05 09:39:01 +0800

    Create SterilizationIndicatorActivePage component

---
+++ frontend/pages/sterilization/indicators/active.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminLayout from "../../../components/AdminLayout";
+
+export default function SterilizationIndicatorActivePage() {
+  return (
+    <AdminLayout>
+      <h1 style={{ fontSize: 18 }}>Идэвхитэй индикатор</h1>
+      <div style={{ color: "#6b7280", fontSize: 13 }}>
+        (Дараагийн алхамд хийнэ)
+      </div>
+    </AdminLayout>
+  );
+}
