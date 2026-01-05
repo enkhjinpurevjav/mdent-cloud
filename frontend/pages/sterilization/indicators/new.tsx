9fb6045abc9e7d1f08dbcf37ccd571b31664318b
enkhjinpurevjav <enkhjinp@gmail.com>
2026-01-05 09:38:23 +0800

    Create new.tsx

---
+++ frontend/pages/sterilization/indicators/new.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminLayout from "../../../components/AdminLayout";
+
+export default function SterilizationIndicatorNewPage() {
+  return (
+    <AdminLayout>
+      <h1 style={{ fontSize: 18 }}>Индикатор үүсгэх</h1>
+      <div style={{ color: "#6b7280", fontSize: 13 }}>
+        (Дараагийн алхамд хийнэ)
+      </div>
+    </AdminLayout>
+  );
+}
