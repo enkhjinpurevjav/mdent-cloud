e63e67c18102b853cd20425c0e4aa33a286d04ac
enkhjinpurevjav <enkhjinp@gmail.com>
2026-01-05 15:21:46 +0800

    Update reports.tsx

--- frontend/pages/sterilization/reports.tsx
+++ frontend/pages/sterilization/reports.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 export default function SterilizationReportsPage() {
   return (
- 
+    <div>
       <h1 style={{ fontSize: 18 }}>Ариутгалын тайлан</h1>
-      <div style={{ color: "#6b7280", fontSize: 13 }}>
-        (Дараагийн алхамд хийнэ)
-      </div>
-    
+      <div style={{ color: "#6b7280", fontSize: 13 }}>(Дараагийн алхамд хийнэ)</div>
+    </div>
   );
 }
