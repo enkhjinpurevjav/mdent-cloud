0bb18d78d038b437b6cf060b221a4f44b2502232
enkhjinpurevjav <enkhjinp@gmail.com>
2026-01-23 16:09:36 +0800

    Update booked.tsx

--- frontend/pages/visits/booked.tsx
+++ frontend/pages/visits/booked.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import AppointmentFiltersBar from "../../components/AppointmentFiltersBar";
 import type { AppointmentFilters, AppointmentRow } from "../../types/appointments";
 
